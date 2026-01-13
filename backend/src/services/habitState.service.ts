94d556b74189f2e6a1133735890e23b8b9a24735
Ekam <bhat0155@algonauinlive.com>
2026-01-12 20:44:10 -0500

    feat: added habit service which returns habit as mentioned in contract

---
+++ backend/src/services/habitState.service.ts
@@ -0,0 +1,39 @@
+import {prisma} from "../config/prisma";
+import { getTodayUTCDate } from "../utils/utc";
+import type { HabitBox, HabitMeta, HabitState } from "../contracts/habitState";
+
+function buildEmptyBoxes(): HabitBox[]{
+    return Array.from({length: 21}, (_, i)=>{
+        return {
+            day: i+1,
+            status: false,
+            canEdit: i===0
+        }
+    })
+}
+
+export async function getHabitStateForUser(habitId: string, userId: string): Promise<HabitState>{
+    const todayUTC = getTodayUTCDate();
+
+    const habit = await prisma.habit.findUnique({
+        where: {userId},
+        select: {
+            id: true,
+            name: true,
+            bestStreak: true,
+            allDone: true,
+            isPublic: true,
+            publicSlug: true
+        }
+    });
+
+   const habitMeta: HabitMeta | null = habit ? habit : null;
+
+   return {
+        habit: habitMeta,
+        todayUTC,
+        checkedInToday: false,
+        currentStreak: 0,
+        boxes: buildEmptyBoxes()
+   }
+}
