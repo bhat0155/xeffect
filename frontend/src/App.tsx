463094ebb857610a3c1c5f8f66759a2aac9657e3
Ekam <bhat0155@algonauinlive.com>
2026-01-17 19:16:58 -0500

    feat: initialised vite, tailwind and dasiyUI

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div>
+    <button className="btn btn-primary">DaisyUI works</button>
+    <div className="card bg-base-100 shadow-xl p-4 mt-4">Card</div>
+   </div>
+  )
+}
+
+export default App
